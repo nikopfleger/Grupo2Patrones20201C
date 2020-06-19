ab3297e60282d201f0a675b35b4521708ce92af3
Nicolas Pfleger <nicolas.pfleger@gmail.com>
2020-06-19 20:43:20 -0300

    Query Implementacion

--- src/myhibernate/Query.java
+++ src/myhibernate/Query.java
@@ -1,17 +1,39 @@
 package myhibernate;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Query
 {
+   private String hql;
+   
+   public Query(String q) {
+	   hql = q;
+   }
+	
    public void setParameter(String pName,Object pValue)
    {
-      // PROGRAMAR AQUI
+	  if ((pValue instanceof String) || (pValue instanceof Number))
+		  hql = hql.replace(":" + pName, pValue.toString());
+	  else
+	  {
+		
+	  }
+      // PROGRAMAR AQUI   
+	   
    }
 
    public <T> List<T> getResultList()
    {
       // PROGRAMAR AQUI
       return null;
+   }
+   
+   public String getHql() {
+	   return hql;
+   }
+   
+   public void setHql(String hql) {
+	   this.hql = hql;
    }
 }
